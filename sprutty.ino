9440b926244cbf2e5a0875c7e9f5ac2ce4551687
supertima <47712277+supertima@users.noreply.github.com>
2021-10-17 15:38:18 +0300

    Update sprutty.ino

--- sprutty.ino
+++ sprutty.ino
@@ -14,14 +14,13 @@ uint8_t sensor1[8] = { 0x28, 0xFF, 0x64, 0x1E, 0x01, 0xA6, 0xAF, 0x6A };
 uint8_t sensor2[8] = { 0x28, 0xFF, 0x64, 0x1E, 0x01, 0xAF, 0xDD, 0x9C };
 uint8_t sensor3[8] = { 0x28, 0xFF, 0x64, 0x1E, 0x01, 0xA0, 0xE8, 0x05 };
 
-
 LiquidCrystal_I2C lcd(0x27,20,4);     //  Here we define symbol display parameters (address of I2C = 0x27, columns = 20, rows = 4)
 
 
-const uint8_t  pinLine = A0;          //  Here we define a constant, which stores pin number for line sensor (needed for engine tachometer)
-      uint32_t strLine = 0;           //  variable for line sensor (needed for engine tachometer)
-                                     
-                                  
+volatile byte revolutions;
+unsigned int rpm;
+unsigned long timeold;
+                              
 void setup()
 {                      
     sensors.begin();                  //  Initialize sensors
@@ -37,14 +36,14 @@ void setup()
     lcd.print("Radiator temp:");      //  Print description on LCD. It is printed in the beginning of the third row.
     lcd.setCursor( 0, 3);             //  Set cursor on first symbol of fourth row. (Numeration begin from "0").
     lcd.print("Engine RPM:");         //  Print description on LCD. It is printed in the beginning of the fourth row.
+      
+    attachInterrupt(0, rpm_count, RISING);  //  Setup an interrupt which activates "rpm" function each time value on first interrupt (D2 pin) rises.
+      
+    revolutions = 0;
+    rpm = 0;
+    timeold = 0; 
 }                                    
 
-
-void printTemperature(DeviceAddress deviceAddress)    // Here we define a universal function, which receives sensor's address as a parameter, checks it's temperature and print's it on LCD. 
-{
-  float tempC = sensors.getTempC(deviceAddress);      // Check what is the temperature on the sensor with the address "deviceAddress" and sets this value to the variable "tempC".
-  lcd.print(tempC);                                   // Print tempC's value on the LCD. Note: this function prints text wherever cursors is. So you have to set cursor's location beforehand. 
-}
 
 
 void loop()
@@ -62,27 +61,28 @@ void loop()
 
   
   // Here we begin our tachometer magic
+  if (revolutions >= 30) { 
+     //Update RPM every 20 counts, increase this for better RPM resolutions, decrease for faster update
+     rpm = 60*1000/(millis() - timeold)*revolutions;
+     timeold = millis();
+     revolutions = 0;
+     lcd.setCursor( 15, 3);             //  Set cursor after the previously printed "Engine RPM:" on LCD.
+     lcd.print(rpm);
+   }
   
-  if(analogRead(pinLine)>400){                             //  если показания датчика линии больше 400 (тёмное поле)
-    if(strLine<millis()){                                  //  если нет переполнения millis(), то ...
+}
 
-      lcd.setCursor( 15, 3);          // Set cursor after the previously printed "Engine RPM:" on LCD.
-      lcd.print("     ");             // Print 5 empty spaces to clear this area on LCD from any previous values.
-      lcd.setCursor( 15, 3);          // Set cursor back after the previously printed "Engine RPM:" on LCD.
-      lcd.print(60000/(millis()-strLine));      // Print RPM value. Here we actually calculate how much time passed from the last time when sensor
-    
-    } strLine = millis();                                  //  присваиваем переменной strLine текущее значение millis();
-    while(analogRead(pinLine)>350){                        //  ждём пока датчик линии не выйдет из тёмного поля
-      if((millis()-strLine)>6000){
 
-        lcd.setCursor( 15, 3);
-        lcd.print("STOP+");
-        }  //  если датчик находится на тёмном  поле дольше 6 секунд, то выводим на индикатор надпись "STOP"
-    }
-  }   if((millis()-strLine)>6000){
-    
-        lcd.setCursor( 15, 3);
-        lcd.print("STOP-");
-        }  //  если датчик находится на светлом поле дольше 6 секунд, то выводим на индикатор надпись "STOP"
+// Function to be activated during interrupt to count rpm's
+void rpm_count()                            
+{
+  revolutions++;
+}
 
+
+// Here we define a universal function, which receives sensor's address as a parameter, checks it's temperature and print's it on LCD. 
+void printTemperature(DeviceAddress deviceAddress)    
+{
+  float tempC = sensors.getTempC(deviceAddress);      // Check what is the temperature on the sensor with the address "deviceAddress" and sets this value to the variable "tempC".
+  lcd.print(tempC);                                   // Print tempC's value on the LCD. Note: this function prints text wherever cursors is. So you have to set cursor's location beforehand. 
 }
