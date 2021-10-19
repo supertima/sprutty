2515cd9296edf557c598c8703b7e217b521a3865
supertima <47712277+supertima@users.noreply.github.com>
2021-10-19 13:37:31 +0300

    Update sprutty.ino

--- sprutty.ino
+++ sprutty.ino
@@ -16,10 +16,8 @@ uint8_t sensor3[8] = { 0x28, 0xFF, 0x64, 0x1E, 0x01, 0xA0, 0xE8, 0x05 };
 
 LiquidCrystal_I2C lcd(0x27,20,4);     //  Here we define symbol display parameters (address of I2C = 0x27, columns = 20, rows = 4)
 
-
-volatile byte revolutions;
 unsigned int rpm;
-double timefunc, timeold;
+double timefunc;
                               
 void setup()
 {                      
@@ -45,46 +43,41 @@ void setup()
     timefunc = 0;                     //  Stores timestamp for the beginning of current calculation cycle
 }                                    
 
-
-
 void loop()
 {
-  sensors.requestTemperatures();      // call "sensors.requestTemperatures()" to issue a global temperature request to all devices on the bus
-  
+  sensors.requestTemperatures();      // call "sensors.requestTemperatures()" to issue a global temperature request to all devices on the bus  
+
   lcd.setCursor( 15, 0);              // Set cursor after the previously printed "Oil temp:" on LCD.
   printTemperature(sensor1);          // Print sensor's 1 temperature on LCD.
-
   lcd.setCursor( 15, 1);              // Set cursor after the previously printed "Engine temp:" on LCD.
   printTemperature(sensor2);          // Print sensor's 2 temperature on LCD.
-
   lcd.setCursor( 15, 2);              // Set cursor after the previously printed "Radiator temp:" on LCD.
   printTemperature(sensor3);          // Print sensor's 3 temperature on LCD.
 
+  if ((micros()-timeprev)>2000000)    // If there is more than 2 seconds since last rev, we consider the engine is stopped
+  {
+     lcd.setCursor( 15, 3);
+     lcd.print("STOP");      
+   }
   
-  if (revolutions >= 5)
-  { 
-     //  Update RPM every 5 counts, increase this for better RPM resolutions, decrease for faster update
-     noInterrupts();
-     timefunc = micros();
-     rpm = (60/((timefunc-timeold)/1000000))*revolutions;
-     timeold = timefunc;
-     revolutions = 0;
-     interrupts();
+  else                                // else we just print RPM's on the display
+  {
      lcd.setCursor( 16, 3);
      lcd.print("   ");
      lcd.setCursor( 15, 3);
-     lcd.print(rpm);
-   }
-  
-  //  This scenario is ran only when there is more than 5 sec. since last 5 revs were counted. thus telling us engine is very likely stopped.
-  else if (micros()-timefunc > 5000000)
-  {
-    lcd.setCursor( 15, 3);
-    lcd.print("STOP"); 
-  }
-  
+     lcd.print(rpm); 
+  }  
 }
 
+
+// Function to be activated during interrupt to count rpm's. We count RPM's inside interrupt as is it is much faster and more precise.
+void rpm_count()                            
+{
+  rpm = 60/((micros()-timeprev)/1000000);   // Math is simple: we calculate how much time passed since previous rev. Knowing that, we can easily get how many RPM's will engine make in 60 seconds.
+  timeprev = micros();
+}
+  
+}
 
 // Function to be activated during interrupt to count rpm's
 void rpm_count()                            
